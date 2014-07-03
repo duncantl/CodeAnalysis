5f9058f3a33cf94343e78d96d0693f3bc1be30e5
Duncan Temple Lang <duncan@r-project.org>
2014-07-03 10:32:20 -0700

    cleaned up the structure  of the expressions for the if removal

---
+++ tests/ifRemoval.R
@@ -0,0 +1,66 @@
+#XXX Fix h - body is list(x)
+h = function(x) x
+
+f =
+function(x)
+{
+   if(FALSE) {
+       print(x)
+   }
+
+   if(TRUE) {
+       g(x)
+   }  else
+       h(x)
+
+   if(FALSE && x > 10) {
+       a(x)
+   }  else 
+       b(x)
+
+}
+
+g =
+function(x)
+{
+   for(i in x) {
+       if(FALSE)
+           print(x)
+       x[i] = x[i] + 1
+   }
+   x
+}
+
+
+ok =
+function(x)
+{
+   if(x > 10)
+       print(x)
+
+   y = numeric(length(x))
+   if(length(x) && TRUE)
+        for(i in seq(along = x)) {
+           if(i < -1)
+               print(x)
+           y[i] = sin(x[i])
+        }
+   y
+}
+
+
+notOK =
+function(x)
+{
+   if(x > 10)
+       print(x)
+
+   y = numeric(length(x))
+   if(length(x) && FALSE)  # this will be removed.
+        for(i in seq(along = x)) {
+           if(i < -1)
+               print(x)
+           y[i] = sin(x[i])
+        }
+   y
+}
