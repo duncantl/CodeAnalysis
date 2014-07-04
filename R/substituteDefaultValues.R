9156112edef6fb661053094c3d4d73f23f173c76
Duncan Temple Lang <duncan@r-project.org>
2014-07-04 11:49:09 -0700

    basics of adding the default values into the body

---
+++ R/substituteDefaultValues.R
@@ -0,0 +1,42 @@
+
+isDefaultValue =
+function(x)
+    !( is.name(x) && as.character(x) == "")
+
+
+substituteDefaultValues =
+function(f, sc = new("Script", as.list(body(f))[-1]), info = getInputs(sc))
+{
+    hasDef = sapply(formals(f), isDefaultValue)
+    vnames = names(formals(f))[hasDef]
+
+    w = lapply(vnames,
+                function(var)
+                    which(sapply(info, function(x) var %in% x@inputs)))
+    names(w) = vnames
+    vals = formals(f)[hasDef]
+    e = mapply(function(id, e)
+                  substitute( v <- e, list(v = as.name(id), e = e)),
+               vnames, vals)
+
+  # now merge these new expressions into the list of expressions.
+    w = unlist(w)
+    w = w[order(w, decreasing = TRUE)]
+
+    gr = split(data.frame(id = names(w), w = w), w)
+
+    els = as.list(body(f))[-1]
+        # now go backwards so the indices won't be affected.            
+    for(i in rev(names(gr))) {
+        pos = as.integer(i)
+        els = c(els[1:(pos-1L)], e[as.character(gr[[i]]$id)], els[pos:length(els)])
+    }
+
+
+    body(f) = substitute({})
+    body(f)[seq(along = els) + 1L] = els
+#    body(f) = structure(c("{", els), class = "{")
+    
+     f
+}
+
