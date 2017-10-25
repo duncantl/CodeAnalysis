e8ef978addde9b87c841aee566504e9114e0f802
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-25 11:19:25 -0700

    funcToScript working for simple case

--- explorations/funcToScript.R
+++ explorations/funcToScript.R
@@ -1,27 +1,29 @@
 #' Convert a function into a script which can be run from command line
 #'
 #' Only handling functions which take character arguments
-funcToScript = function(f, script = paste0(deparse(f), ".R"))
-{
-
-    # Assume it's not an anonymous function
-    fname = deparse(f)
-
+#'
+#' @param f function
+#' @param fname name of the function. Must provide if f is anonymous
+#' @param script file to write the script to
+funcToScript = function(f
+    , fname = deparse(substitute(f))
+    , script = paste0(fname, ".R")
+){
     # Print everything right to the script
     sink(script, type = "output")
 
     cat(
 "#!/usr/bin/env Rscript\n"
-, "Auto generated at ", as.character(Sys.time())
+, "# Auto generated at ", as.character(Sys.time())
 , "\n\n\n"
 , fname, " <- "
 )
-print(f)
+    print(f)
     cat("
-args <- commandArgs(trailingOnly = TRUE)
+args <- as.list(commandArgs(trailingOnly = TRUE))
 do.call(", fname, ", args)"
 )
-
+    sink()
 }
 
 
---
+++ explorations/test_funcToScript.sh
@@ -0,0 +1,2 @@
+Rscript funcToScript.R
+Rscript fileRowSums.R 123.txt sums.txt
