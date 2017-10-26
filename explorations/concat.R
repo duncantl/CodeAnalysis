7baf4ddfe5e36f54c53298ad0080b91eb423155f
Duncan Temple Lang <duncan@r-project.org>
2017-10-25 17:27:00 -0700

    additional examples

--- explorations/concat.R
+++ explorations/concat.R
@@ -11,6 +11,29 @@ for(i in 1:length(x)) {
    ans[i] = f(x[i])
 }
 
+ans = integer()
+for(i in 2:length(x)) {
+   ans[i] = f(x[i], x[i-1])
+}
+
+
+# start with a single value but still adding to the end of  a vector.
+# Looping from 2 onwards.
+ans = 0L
+for(i in 2:length(x)) {
+   ans[i] = f(x[i], ans[i-1])
+}
+
+ans = rep(NA, length(x))
+ans[1] = 0L
+for(i in 2:length(x)) {
+   ans[i] = f(x[i], ans[i-1])
+}
+# Better
+for(i in  (seq( along = x[-1])  + 1)) {
+   ans[i] = f(x[i], ans[i-1])
+}
+
 
 # Parallel vectors - mapply.
 ans = c()
