17a3ddcb533b5889fb5c620b8bae76adbc45c082
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-31 13:30:28 -0700

    second loop -> apply example

--- explorations/loop_to_apply/loops.R
+++ explorations/loop_to_apply/loops.R
@@ -4,22 +4,45 @@
 # Original:
 ans = rep(NA, length.out = length(x))
 for(i in seq_along(x)) {
-   ans[i] = f(x[i])
+    ans[i] = f(x[i])
 }
 
-# Desired Output:
+# Transformed:
 ans = sapply(x, f)
 
 
-# Example 2 - Second assignment inside loop, not used inside loop
+# Example 2 - Intermediate variable used inside loop
+#
+# I often use intermediate variables to make my code easier to read and
+# debug. This may be more complex than we care to go. It's interesting
+# because the solution involves the intermediate step of reducing the
+# problem to something equivalent to Example 1.
 ############################################################
 
 # Original:
 ans = rep(NA, length.out = length(x))
 for(i in seq_along(x)) {
-   current = f(x[i])
-   ans[i] = current
+    tmp = f(x[i])
+    ans[i] = g(tmp)
 }
-g(current)
 
 
+# Intermediate:
+loopbody = function(xi)
+{
+    tmp = f(xi)
+    g(tmp)
+}
+ans = rep(NA, length.out = length(x))
+for(i in seq_along(x)) {
+    ans[i] = loopbody(x[i])
+}
+
+
+# Transformed:
+loopbody = function(xi)
+{
+    tmp = f(xi)
+    g(tmp)
+}
+ans = sapply(x, loopbody)
