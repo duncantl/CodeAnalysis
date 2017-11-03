9fa6024b74090f5d742a64d6de040d47952bdd32
Duncan Temple Lang <duncan@r-project.org>
2017-11-02 19:43:48 -0700

    real example from Scott

--- Topics/extractFuns/eg2.R
+++ Topics/extractFuns/eg2.R
@@ -13,6 +13,39 @@ sapply(body(fun), isFunction)
 
 o = extractFunctions(fun)
 
+e = new.env()
+invisible(sapply(o$externalFunctions, function(f) eval(f, e)))
 
 
+# Now that we have these functions, we find out which global variables
+# these refer to. 
 
+globals = lapply(ls(e), function(var) codetools::findGlobals(get(var, e), FALSE))
+names(globals) = ls(e)
+
+w = sapply(globals, function(x) length(x$variables)) >  0
+sapply(globals[w], `[[`, "variables")
+
+
+# So we can now use mkGlobalLocals() to rewrite the functions to add the global
+# variables as
+
+# mkGlobalLocals()
+
+################################
+#
+# For reasons totally unrelated to this example, let's find the
+# global variables in these functions
+
+intrinsics = c("{", "(", "[", "if", "return", "=", "<-") 
+
+setdiff(codetools::findGlobals(e$KepCalc), intrinsics)
+# [1] "-"   "*"   "/"   "+"   ">"   "max" "min"
+# So only math operations and simple min and max.
+# So we could translate this to C, if we knew the types of the inputs,
+# i.e.  Krp[i] where Krp is a numeric vector.
+
+
+setdiff(unlist(globals[grep("Calc", ls(e), value = TRUE)]), c(intrinsics, ls(e)))
+# Mix of variables and functions.
+# The functions include paste0, which, tail, nrow, length, vector
