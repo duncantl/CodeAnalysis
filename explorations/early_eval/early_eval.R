a4795c79633c27f10cbdd88930f61320e0018c8f
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-11-13 08:31:08 -0800

    forgot to add notes in early eval

--- explorations/early_eval/early_eval.R
+++ explorations/early_eval/early_eval.R
@@ -1,3 +1,5 @@
+library(codetools)
+
 early_eval = function(expr)
 {
     expr = as.expression(expr)
@@ -12,10 +14,14 @@ mini = new.env(parent = emptyenv())
 # Nothing here yet:
 ls(mini)
 
-# Darn, I hoped it wouldn't find + in the base package
+# This works, discovers + in base
 eval(1 + 2, envir = mini)
+
+# Same
+local(1 + 2)
 
 # And it finds this in the stats package
 eval(median(c(1, 2)), envir = mini)
 
 # Then I need to modify the search path.
+# docs say that it's not possible to detach base.
