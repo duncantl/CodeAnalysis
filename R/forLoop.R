55fa96f6f71f53dea2eecec6193fd2afb1c2583b
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-07-08 14:11:18 -0700

    first two tests pass

--- R/forLoop.R
+++ R/forLoop.R
@@ -3,41 +3,66 @@ find_var2 = function(node, varname){
                         is(x, "Symbol") && x$value == varname)
 }
 
+assigns_into_var = function(node, varname){
+    finds = rstatic::find_nodes(node, function(x)
+                        is(x, "Assign") && 
+                        is(x$write, "Symbol") &&
+                        x$write$value == varname
+                    )
+    0 < length(finds)
+}
+
+
 
 #' Determine If a Loop Can Be Made Parallel
 #'
 #' and tell the user why the loop is parallel or not.
 #'
 #' A loop can be made parallel if the order of the iterations do not matter.
-#' This function errs on the side of being conservative; if it's not clear whether a loop is parallel or not, it will say that it is not.
+#' A loop is not parallel if it has a true dependency on loop iterations.
+#' There might be many things that stop a loop from being parallel.
+#' This functions stops and returns as soon as it finds one reason.
+#' The design errs on the side of being conservative; if it's not clear whether a loop is parallel or not, it will say that it is not.
 #'
 #' @param forloop for loop language object
-#' @param checkIterator logical check that the iterator is guaranteed
-#' @param uniqueFuncs
+#' @param checkIterator logical check that the iterator is a function call that is guaranteed to produce only unique values.
+#'      This should be TRUE if you really want to be sure that the loop is parallelizable.
+#' @param uniqueFuncs names of functions that will produce unique values.
 #' @return list with the following elements:
 #'      - parallel (logical) can the for loop be parallel?
 #'      - reason (character) human readable message for why the loop is or is not parallel
 #'      - reasonCode (character) short version of reason, for programming
-parLoop = function(forloop, checkIterator = FALSE, uniqueFuncs = c("seq", ":",)
+parLoop = function(forloop, checkIterator = FALSE, uniqueFuncs = c("seq", ":", "unique"))
 {
 
     forloop = rstatic::to_ast(forloop)
+    body = forloop$body
 
-    if(!is(l, "For")){
+    if(!is(forloop, "For")){
         stop("Not a for loop.")
     }
 
-    deps = CodeDepends::getInputs(as_language(forloop$body))
-
+    deps = CodeDepends::getInputs(as_language(body))
     changed = c(deps@outputs, deps@updates)
+
     if(length(changed) == 0){
         return(list(
             parallel = TRUE,
-            reason = "Loop does not change any variables.",
+            reason = "loop does not define or update any variables",
             reasonCode = "NO_CHANGE"
         ))
     }
 
+    global_updates = intersect(deps@inputs, deps@updates)
+    for(v in global_updates){
+        if(assigns_into_var(body, v)){
+            return(list(
+                parallel = FALSE,
+                reason = sprintf("read after write dependency on variable `%s`", v),
+                reasonCode = "RAW"
+            ))
+        }
+    }
 
 }
 
@@ -130,12 +155,13 @@ if(FALSE){
 #})
 # Maybe relevant: https://github.com/r-lib/R6/issues/36
 
-
+q = quote_ast(assign("x", 1))
 
 # Check when y is considered both an input and an output
 getInputs(quote({
-    z = 5
+    assign("x", 50)
     y = bar(y)
+    z = 5
 }))
 
 # An update for CodeDepends can be from x[i] = ... or x = ... if it sees that x is defined
