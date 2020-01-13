8f5648ea9e11d1880332332edb7dd3247078fd0e
Duncan Temple Lang <duncan@r-project.org>
2020-01-13 09:52:54 -0800

    these should move to CodeDepends - later.  getVarGraph() computes the graph of variable dependencies up to a definition. Will add global variables of functions

---
+++ R/getPackages.R
@@ -0,0 +1,155 @@
+setGeneric("getPackages",
+           function(code, ...)
+           standardGeneric("getPackages"))
+
+setMethod("getPackages",
+          "character",
+          function(code, ...) {
+
+              if(file.exists(code))
+                  sc = readScript(code)
+              else
+                  sc = readScript(parse(text = code))
+              
+              getPackages(sc, ...)
+          })
+
+setMethod("getPackages",
+          "expression",
+          function(code, ...) {
+              i = getInputs(code)
+              getPackages(i, ...)
+          })
+
+setMethod("getPackages",
+          "Script",
+          function(code, ...) 
+ {
+     getPackages(as(code, "ScriptInfo"), ...)
+ })
+
+setMethod("getPackages",
+          "ScriptInfo",
+          function(code, counts = FALSE, ...) 
+          {
+              ans = unlist(sapply(code, slot, "libraries"))
+              if(counts)
+                  table(ans)
+              else
+                  unique(ans)
+})
+
+
+# Should be in CodeDepends, not here.
+setAs("expression", "ScriptInfo",
+      function(from)
+         new("ScriptInfo", lapply(from, getInputs)))
+
+getToplevelVariables =
+function(code, ...)
+{
+    info = as(code, "ScriptInfo")
+    unique(unlist(c(lapply(info, slot, "outputs"), lapply(info, slot, "updates"))))
+}
+    
+
+################
+
+# Need methods as we do different things with different inputs,
+# and not everything should be coerced to a ScriptNodeInfo as below.
+
+setGeneric("getOutputVars",
+           function(x, ...)
+              standardGeneric("getOutputVars"))
+
+tmp = function(x, merge = TRUE)
+{
+    i = as(x, "ScriptNodeInfo")
+    ans = i@outputs
+    if(merge)
+        ans = c(ans, i@updates)
+    ans
+}
+
+setMethod("getOutputVars", "ScriptNodeInfo", tmp)
+setMethod("getOutputVars", "call", tmp)
+setMethod("getOutputVars", "<-", tmp)
+setMethod("getOutputVars", "=", tmp)
+
+setMethod("getOutputVars", "character",
+          function(x, merge = TRUE) {
+              if(file.exists(x))
+                  e = parse(x)
+              else
+                  e = parse(text = x)
+              getOutputVars(e, merge)
+          })
+
+setMethod("getOutputVars", "expression",
+          function(x, merge = TRUE) {
+              if(length(x) == 1)
+                  getOutputVars(as(x[[1]], "ScriptNodeInfo"), merge)
+              else
+                  getOutputVars(as(x, "ScriptInfo"), merge)                  
+          })
+
+setMethod("getOutputVars", "ScriptInfo",
+          function(x, merge = TRUE) {
+              unique(unlist(lapply(x, getOutputVars, merge)))
+          })
+
+setGeneric("getInputVars",
+           function(x, ...)
+            standardGeneric("getInputVars"))
+
+getInputVars =
+function(x, merge = TRUE, functions = FALSE)
+{
+    i = as(x, "ScriptNodeInfo")
+    ans = i@inputs
+    if(merge)
+        ans = c(ans, i@updates)
+    if(functions)
+        ans = c(ans, names(i@functions))
+    ans
+}
+
+setAs("character", "ScriptNodeInfo",
+      function(from) {
+          as(parse(text = from), "ScriptNodeInfo")
+      })
+
+
+
+
+
+
+###################
+
+getVarGraph =
+function(var, sc, asGraph = TRUE,
+         idx = getVariableDepends(var, sc, asIndex = TRUE))
+{
+    df = createVarGraph(sc[i], sc)
+    if(asGraph)
+        graph_from_data_frame(df)
+    else
+        df
+}
+
+createVarGraph =
+function(cmds, script = NULL)
+{
+    info = lapply(cmds, getInputs)
+    do.call(rbind, lapply(info, inputOutputEdges))
+}
+
+inputOutputEdges =
+function(info)    
+{
+    ins = unique(info@inputs)
+    outs = unique(c(info@outputs, info@updates))
+#    if(length(outs) > 1)
+#        warning("more than one output for ", paste(deparse(info@code), sep = " "))
+    expand.grid(input = ins, output = outs, stringsAsFactors = FALSE)
+}
