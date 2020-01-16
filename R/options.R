97e55f3e2fbbfd4a140c517f546399609c90b9c8
Duncan Temple Lang <duncan@r-project.org>
2020-01-15 17:39:49 -0800

    update for  instead of  for rstatic nodes and for find_nodes returning the nodes, not indices any more

--- R/options.R
+++ R/options.R
@@ -7,24 +7,25 @@ function(code, asNodes = FALSE)
     code = to_ast(code)
     idx = find_nodes(code, isUsedOption)
     if(asNodes)
-        lapply(idx, function(i) code[[i]])
+        idx
     else
-        lapply(idx, function(i) getUsedOptionName(code[[i]]))
+        sapply(idx, getUsedOptionName)
 }
 
 
 isUsedOption =
 function(node)
 {
-   is(node, "Call") && is(node$fn, "Symbol") && ((node$fn$name == "getOption" || (node$fn$name == "options" && length(node$args) > 0)) ) ||  (node$fn$name %in% c("$", "[[", "[") && is(node$args[[1]], "Call") && is(node$args[[1]]$fn, "Symbol") && node$args[[1]]$fn$name == "options")
+    is(node, "Call") && is(node$fn, "Symbol") && ((node$fn$value == "getOption" || (node$fn$value == "options" && length(node$args) > 0)) ) ||
+        (node$fn$value %in% c("$", "[[", "[") && is(node$args[[1]], "Call") && is(node$args[[1]]$fn, "Symbol") && node$args[[1]]$fn$value == "options")
 }
 
 
 getUsedOptionName =
 function(node)
 {
-    if(node$fn$name %in% c("options", "getOption")) {
-        els = node$args
+    if(node$fn$value %in% c("options", "getOption")) {
+        els = node$args$contents
     } else if(node$fn$name == "$") {  # must be  options()$... or [[...]] or [
        return(node$args[[2]]$name)
     } else # [[ or [
