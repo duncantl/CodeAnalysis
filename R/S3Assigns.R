9970aa3e6c8a0b289534ca0533979f264d73ae25
Duncan Temple Lang <duncan@r-project.org>
2020-01-15 20:10:57 -0800

    S3Assignments function updates

--- R/S3Assigns.R
+++ R/S3Assigns.R
@@ -9,9 +9,9 @@ function(fun, asNode = FALSE)
   i = find_nodes(fun, isS3Assign)
   if(length(i)) {
       if(asNode)
-          return(lapply(i, function(idx) fun[[idx]]))
+          i
       else
-          return(lapply(i, function(idx) extractS3Class(fun[[idx]])))
+          return(lapply(i, function(node) extractS3Class(node)))
   } else
      NULL
 }
@@ -19,18 +19,48 @@ function(fun, asNode = FALSE)
 isS3Assign =
 function(n)      
 {
-  is(n, "Call") && is(n$fn, "Symbol") && n$fn$value == "class<-"
+  (is(n, "Call") && is(n$fn, "Symbol") && n$fn$value == "class<-") ||
+      (is(n, "Call") && is(n$fn, "Symbol") && n$fn$value == "structure" && "class" %in% names(n$args$contents))
 }
 
 extractS3Class =
-    # node is typically going to be a Call to class<-
-    # and we will want the second argument and to get the literals
+    # node is typically going to be a Call to
+    #  class<- "val"
+    #  class <- c("val1", "val2")
+    #  class <- c("val1", sym)    
+    #  class <- foo(...)
+    #  structure(..., class = "value")
+    #  structure(..., class = c("value", "value2"))
+    #  structure(..., class = c("value", variable))
+    #  structure(..., class = foo("value", variable))    
+    #
 function(node)
 {
-
-    i =  find_nodes(node$args[[2]], is, "Character")
-    if(length(i))
-        unlist(lapply(i, function(idx) node$args[[2]][[idx]]$value))
-    else
-        character()
+    if(node$fn$value == "structure") {
+        #XXX node$args[["class"]]  doesn't work. Fix rstatic.
+        i = match("class", names(node$args))
+        if(is.na(i)) {
+            warning("problems with S3 class assignment")
+            return(character())
+        }
+        tmp = node$args$contents[[i]]
+        if(is(tmp, "Call"))
+            sapply(tmp$args$contents, function(x) if(is(x, "Character")) x$value else NA)
+        else if(is(tmp, "Character"))
+            tmp$value
+        else
+            NA
+    } else {
+        # call to class<-() so right hand size.
+        # Could be  string, c(str, str), foo(...)
+        args = node$args[[2]]
+        if(is(args, "Call"))
+            sapply(args$args$contents, function(x) if(is(x, "Character")) x$value else NA)
+        else if(is(args, "Character"))
+            args$value
+        else if(is(args, "Symbol"))
+            NA
+        else
+            stop("don't yet recognize this class assignment")
+    }
 }
