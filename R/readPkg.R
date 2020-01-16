8fd084dfe4ecc8d66413cb054744e15ea20c46a3
Duncan Temple Lang <duncan@r-project.org>
2020-01-15 17:37:16 -0800

    full names of parameters in ls() and list.files() from R CMD check warnings

--- R/readPkg.R
+++ R/readPkg.R
@@ -1,12 +1,12 @@
 
 loadCode =
-function(dir, env = new.env(), files = list.files(dir, pattern = "\\.[RrsSqQ]$", full = TRUE))
+function(dir, env = new.env(), files = list.files(dir, pattern = "\\.[RrsSqQ]$", full.names = TRUE))
 {
     env$.functionFileMap = data.frame(funcName = character(), file = character())
     lapply(files, function(f) {
-                     cur = ls(env, all = TRUE)
+                     cur = ls(env, all.names = TRUE)
                      source(f, env)
-                     new = setdiff(ls(env, all = TRUE), cur)
+                     new = setdiff(ls(env, all.names = TRUE), cur)
                      env$.functionFileMap = rbind(env$.functionFileMap, data.frame(funcName = new, file  = rep(f, length(new))))
                   })
     env
