3e29b9bc594ad11356918545a8dd3a61bc44306b
Duncan Temple Lang <duncan@r-project.org>
2021-09-19 15:16:27 -0700

    mostly working

--- explorations/ifAnalysis/matchCall.R
+++ explorations/ifAnalysis/matchCall.R
@@ -4,12 +4,13 @@ function(call, def, expand.dots = TRUE)
     paramNames = pnames = names(formals(def))
 
     dotsPos = match("...", paramNames)
-browser()    
+#browser()    
     args = call$args$contents
-    argNames = names(args)
+    origArgNames = argNames = names(args)
     m = match(argNames, pnames, 0)
     # if we have matches, take those out of pnames
-    pnames = pnames[-m]
+    if(any(m != 0))
+        pnames = pnames[-m]
 
     if(!is.na(dotsPos) && dotsPos <= length(pnames))
         pnames = pnames[ -c( dotsPos:length(pnames)) ]
@@ -27,9 +28,27 @@ browser()
     w = (argNames == "")
     argNames[w] = pnames[seq_len(sum(w))]
     names(args) = argNames
+
+    browser()
+    w2 = !(argNames %in% paramNames)
+    if(any(w2)) {
+        if(is.na(dotsPos))
+            stop("extra arguments: ", paste(argNames[w2], collapse = ", "))
+        if(!expand.dots) {
+            tmp = args[w2]
+            args = args[!w2]
+            args$"..." = tmp
+        }
+    }
     
+    # change the order to put them in the order of the parameters/formals in the function definition
     m = match(paramNames, argNames, 0)
-    call$args$contents = args[m]
+    nargs = c(args[m], args[m == 0])
+    na = is.na(names(nargs))
+    names(nargs)[na] = ""
+    
+    empty = sapply(nargs, is, "EmptyArgument")
+    call$args$contents = nargs[!empty]
     
     call
 }
@@ -53,5 +72,15 @@ if(FALSE) {
 
 
     foo = function(x, y = 2, ..., .na.rm = TRUE){}
-    f = to_ast(quote(foo(, 3, .na.r = FALSE,  other = 1)))
+    f = quote(foo(, 3, .na.r = FALSE,  other = 1))
+    fa = to_ast(f)
+    MatchCall(fa, foo)
+    match.call(foo, f, expand.dots = TRUE)
+    match.call(foo, f, expand.dots = FALSE)    
+
+
+    Sum = function (..., na.rm = FALSE)  .Primitive("sum")
+    g = quote(Sum(x, y = 1:10, z, .na.rm = TRUE))
+    match.call(Sum, g)
+    MatchCall(to_ast(g), Sum)
 }
