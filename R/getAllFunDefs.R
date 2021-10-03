e2108a24dbb96c4c94e5ad31cd6927b8e8157fec
Duncan Temple Lang <duncan@r-project.org>
2021-10-03 13:15:24 -0700

    basics of getting names to which functions are assigned, but too simple. Anonymous functions in calls

--- R/getAllFunDefs.R
+++ R/getAllFunDefs.R
@@ -24,20 +24,49 @@ function(recursive = TRUE)
 {
     defs = list()
 
+    assignTo = character()
+    
     leaf = function(e, w) {
 #        message("leaf: ", class(e), e)
         if(typeof(e) == "expression")
-           w$call(e, w)
+            w$call(e, w)
        NULL
    }
     addFun = function(e, w) {
         defs[[ length(defs) + 1L ]] <<- e
+        id = getAsName(assignTo[[1]])
+        browser()        
+        names(defs)[length(defs)] <<- id
         if(recursive) {
             walkCode(e[[2]], w)
             walkCode(e[[3]], w)
         }
         NULL
     }
+
+    pushAssign = function(val) 
+        assignTo <<- c(val, assignTo)
+    popAssign = function(val) 
+        assignTo <<- assignTo[-1]    
+
+    
+    call = function(x, w){
+             message("call: ", class(x), " ", x)
+             if(isIfFalse(x))
+                 return()
+
+             isAssign = is.name(x[[1]]) && as.character(x[[1]]) %in% c("=", "<-")
+             if(isAssign) 
+                 pushAssign(x[[2]])
+
+             
+             for(ee in as.list(x))
+                 if(!missing(ee))
+                     walkCode(ee, w)
+             if(isAssign)
+                 popAssign()
+           }
+        
         
     list(handler = function(e, w) {
 #        message("handler: ", class(e), e)        
@@ -47,14 +76,16 @@ function(recursive = TRUE)
                           NULL
                   },
          leaf = leaf,
-         call = function(x, w){
-             message("call: ", class(x), " ", x)
-             if(isIfFalse(x))
-                 return()
-             
-                     for (ee in as.list(x))
-                         if (!missing(ee))
-                             walkCode(ee, w)
-                 },
+         call = call,
          .funs = function() defs)
+}
+
+getAsName =
+function(e)
+{
+    switch(typeof(e),
+           "character" = e,
+           "symbol" = as.character(e),
+           "call" = paste(deparse(e), collapse = ""),
+           "")
 }
