b5220206c7cf29e3586521851b38153eafe692c4
Duncan Temple Lang <duncan@r-project.org>
2021-10-10 13:36:32 -0700

    handle :: and ::: and also additional functions that take a function object

--- R/getGlobals.R
+++ R/getGlobals.R
@@ -20,6 +20,8 @@ getGlobals =
     #
     # Todo:  process nested function definitions and determine their local variables
     #
+    #    # XXX Doesn't include [[<-, [<-, $<- functions.
+    #
     # When run on compileFunction(), we find ir and nenv as globals.
     # nenv is real as it used before it is defined.
     #  ir appears as a global because its is referenced in a function definition
@@ -37,6 +39,10 @@ getGlobals =
     #
     #
     # Doesn't identify do.call, optim, etc.  But for optim, we have findCallsParam().
+    #
+    #
+    #
+    # Intentionally doesn't include for, if, while, { as functions which findGlobals() does.
     #
 function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE, 
          skip = c(".R", ".typeInfo", ".signature", ".pragma", ".Internal", ".Primitive"),  #XX we probably want to process the arguments within .Internal except the first which is the call.
@@ -86,14 +92,20 @@ function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE,
       e2 = match.call(def, e)
       i = switch(funName,
                  do.call = match("what", names(e2)),
-                 aggregate = if(length(e2) > 3) 4 else NA, # taking some liberties here matching not by name but known position for methods. See ?aggregate.
+          aggregate = if(length(e2) > 3) 4 else NA, # taking some liberties here matching not by name but known position for methods. See ?aggregate.
+          Filter=,
+          Negate=,
+          Find=,
+          Position=,
+          Map=,
+          Reduce=,
                  body = 2L,
                  match.call = 2L,
                  formals = 2L,
                  grep("fun$", names(e2), ignore.case = TRUE)) # match(c("fun", "FUN"), names(e2)))
 
-      if(!is.na(i) && (is.character(e2[[i]]) || is.name(e2[[i]]) || isColonCall(e2[[i]]))) {
-          if(!((val <- deparse(e2[[i]])) %in% localVars))  # as.character rather than deparse()
+      if(!is.na(i) && (is.character(tmp <- e2[[i]]) || is.name(tmp) || isColonCall(tmp))) {
+          if(!((val <- deparse(tmp)) %in% localVars))  # as.character rather than deparse()
               addFunName(val)
           e2[-i]
       } else
@@ -125,21 +137,20 @@ function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE,
           localVars <<- c(localVars, as.character(e[[2]]))
 
       if(is.call(e)) {
-          if(is.call(e[[1]])) {  # e.g. x$bob()
-              fn = e[[1]]
+          if(is.call(fn <- e[[1]])) {  # e.g. x$bob()
+#              fn = e[[1]]
               if(is.name(fn[[1]]) && isColonCall(fn)) { ### (as.character(fn[[1]]) == "::" || as.character(fn[[1]]) == ":::")) {
                   addFunName(deparse(fn))
                   e = e[-1]
               }
               return(lapply(e, fun,  w))
-           }
+          }
 
-          funName = as.character(e[[1]])
+          funName = as.character(fn) # e[[1]])
 
           if(funName == "::" || funName == ":::") {
               addFunName(deparse(e))
               return(NULL)    # Return or keep going?  Return or will get pkg/first element of :: in variables.
-
           } else if(funName == "function") {
                #XXX Should be able to get the name of this if it is available.
               subFunInfo[[length(subFunInfo)+1L]] <<- getGlobals(e, expressionsFor, skip = skip, .ignoreDefaultArgs = .ignoreDefaultArgs)
@@ -150,17 +161,16 @@ function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE,
               if(is.name(e[[2]]))
                   curAssignName <<- c(as.character(e[[2]]), curAssignName)
 
-              if(is.call(e[[3]]) && is.name(e[[3]][[1]]) && (as.character(e[[3]][[1]]) == "::" || as.character(e[[3]][[1]]) == ":::") && is.name(e[[3]][[2]]) && is.name(e[[3]][[3]])) {
-                  vars <<- c(vars, deparse(e[[3]]))
+              if(is.call(tmp <- e[[3]]) && is.name(tmp2 <- tmp[[1]]) && (as.character(tmp2) == "::" || as.character(tmp2) == ":::") && is.name(e[[3]][[2]]) && is.name(e[[3]][[3]])) {
+                  vars <<- c(vars, deparse(tmp))
               } else
-                  fun(e[[3]], fun)
+                  fun(tmp, fun)
               
               e = e[-3]
-              if(is.name(e[[2]]))              
+              if(is.name(tmp <- e[[2]])) {             
                   curAssignName = curAssignName[-1]
-              
-              if(is.name(e[[2]]))
-                  localVars <<- c(localVars, as.character(e[[2]]))
+                  localVars <<- c(localVars, as.character(tmp))
+              }
               
            } else {
               if(funName %in% skip) {
@@ -186,9 +196,9 @@ function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE,
           els = as.list(e)[-1]
           if(funName == "$") # remove the literal name
               els = els[-2]
-          else if (funName %in% c("apply", "eapply", "sweep", "sapply", "lapply", "vapply", "mapply", "tapply", "by", "aggregate", "do.call", "match.fun", "kronecker", "outer", "sweep", "formals", "body", "match.call") || grepl("apply", funName, ignore.case = TRUE)) {
+          else if (funName %in% c("apply", "eapply", "sweep", "sapply", "lapply", "vapply", "mapply", "tapply", "by", "aggregate", "do.call", "match.fun", "kronecker", "outer", "sweep", "formals", "body", "match.call", "Map", "Reduce", "Filter", "Negate", "Find", "Position") || grepl("apply", funName, ignore.case = TRUE)) 
                 els = procIndirectFunCall(e, funName)
-          }
+
            
           lapply(els, fun, w)
           if(popFuns)
@@ -213,19 +223,25 @@ function(f, expressionsFor = character(), .ignoreDefaultArgs = FALSE,
              } else {
                  #                 browser()
                  #                 if(name  == "xml2" || name == "codetools") browser()
-                if(name  == "utils") browser()
+# XXXX
+                 #  If we have this debugging step,
+                 # system.time(getGlobals(funs2$.install_packages)$variables)
+                 # takes 5.34 seconds. So a factor of 10
+                 # Take it out, we get down to .443 seconds
+#       if(name  == "utils") browser()
+                 # How would we insert this and ensure we can identify it easily as a debug statement and remove it
+                 # or have it as a no-op
                  vars <<- c(vars, name)
              }
-
              
              if(length(curFuns))
                  sapply(curFuns, function(id)
                                     varsByFun[[id]] <<- c(varsByFun[[id]], name))
-          }
+         }
        } else if(typeof(e) == "externalptr") {
            
        }  else
-          lapply(as.list(e)[-1], fun, w)
+             lapply(as.list(e)[-1], fun, w)
   } # end of fun = function() {}
 
   if(is.call(f) && as.character(f[[1]]) == "function") 
@@ -288,7 +304,7 @@ else
 
 isColonCall =
 function(e)      
-   is.call(e) && is.name(e[[1]]) && as.character(e[[1]]) %in% c("::", ":::")
+   is.call(e) && is.name(e[[1]]) && ((tmp <- as.character(e[[1]])) == "::" || tmp ==  ":::")
 
 
 getGlobalFunctions =
