a05ce469b6a94dbf62d5af386abdeaed07bd3f9b
Duncan Temple Lang <duncan@r-project.org>
2021-10-10 13:39:48 -0700

    a collection of functions to query information about the code, many using walkCode from codetools

---
+++ R/callsTo.R
@@ -0,0 +1,44 @@
+# find the calls to given functins
+
+mkCallWalker =
+function(funNames)
+{
+    calls = list()
+
+    leaf = function(x, w, ...) {
+        ty = typeof(x)
+        if(ty == "pairlist") {
+            lapply(x, walkCode, w)
+            return(NULL)
+        } else if(ty == "closure") {
+            walkCode(formals(x), w) # lapply(formals(x), walkCode, w)
+            walkCode(body(x), w)
+            return(NULL)
+        } 
+    }
+
+    call = function(x, w) {
+        isName = is.name(x[[1]])
+        if(isName && as.character(x[[1]]) %in% funNames) {
+            calls[[length(calls) + 1L]] <<- x
+        }
+
+        els = as.list(x)
+        if(isName && as.character(x[[1]]) %in% c(".Internal", ".Primitive")) 
+            els = els[-2]
+
+        
+        for (ee in els)
+            if (!missing(ee))
+                walkCode(ee, w)
+    }    
+
+    list(handler = function(x, w) NULL, leaf = leaf, call = call, ans = function() calls )
+}
+
+findCallsTo =
+function(code, funNames, walker = mkCallWalker(funNames))
+{
+    walkCode(code, walker)
+    walker$ans()
+}
---
+++ R/packageAnalysis.R
@@ -0,0 +1,239 @@
+funLength =
+    #
+    # This currently just computes the number of top-level expressions
+    # See numTerms for the complete breakdown.
+    #
+    #
+    #
+function(f)
+{
+    b = body(f)
+    if(is(b, "call"))
+        1L
+    else
+        length(b) - 1L # for the {
+}
+
+
+astnumTerms = numTerms =
+    #
+    # The number of nodes in the AST.
+    #
+    #
+function(x)
+{
+  length(rstatic::find_nodes(rstatic::to_ast(x), function(x) TRUE))
+}
+
+
+# walkCode.  No documentation for the model
+#  if language object -
+#     if first element symbol or character
+#        call handler
+#        get result
+#        if not NULL call the result as a function
+#          otherwise call  call() element
+#     otherwise call call()
+#  otherwise call leaf()
+
+
+mkCounter =
+function(self = FALSE, ctr = 0L)
+{
+    els = list()
+    leaf = function(x, w, ...) { #print((x));
+        ty = typeof(x)
+        if(ty == "pairlist") {
+            lapply(x, walkCode, w)
+            return(NULL)
+        } else if(ty == "closure") {
+            walkCode(formals(x), w) # lapply(formals(x), walkCode, w)
+            walkCode(body(x), w)
+            if(!self) return(NULL)
+        }
+
+         ctr <<- ctr+1L
+         els[[ctr]] <<- x
+
+        NULL
+    }
+    call = function(x, w) { if(self) w$leaf(x)
+                            for (ee in as.list(x))
+                                if (!missing(ee))
+                                    walkCode(ee, w)
+                        }
+    list(handler = function(x, w) NULL ,
+         call = call,
+         leaf = leaf,
+         .result = function() ctr,
+         .els = function()els)
+}
+
+numTerms =
+function(x, ctr = mkCounter())
+{
+#    if(is.function(x)) {
+#        walkCode()
+#    }
+    walkCode(x, ctr)
+    ctr$.result()
+}
+
+
+findS3ClassDefs =
+function(x, ...)
+   UseMethod("findS3ClassDefs")
+
+findS3ClassDefs.character =
+function(x, ...)
+    findS3ClassDefs(parse(x), ...) # deal with vector, directory.
+
+findS3ClassDefs.expression = findS3ClassDefs.function =
+function(x, ...)
+    findS3ClassDefs(to_ast(x), ...)
+
+
+findS3ClassDefs.ASTNode =
+function(x, asNodes = FALSE, ...)
+{
+    nodes =  find_nodes(x, isS3ClassSetNode)
+    if(asNodes)
+        return(nodes)
+
+    sapply(nodes, getS3ClassInfo)
+}
+
+getS3ClassInfo =
+function(x)
+{
+    switch(x$fn$value,
+           "class<-" = paramValue(x$args$contents$value),
+           "structure" = paramValue(x$args$contents$class))
+}
+
+
+isS3ClassSetNode = 
+function(x)
+{
+    # Add attr(, "class") = ...
+   (is(x, "Replacement") && is(x$read, "Call") && is_symbol(x$read$fn, "class<-")) ||
+      is(x, "Call") && is_symbol(x$fn, "structure") && "class" %in% names(x$args$contents)
+}
+#  (is(x, "Replacement") && (is_symbol(x$fn, "class<-"))) ||
+
+
+
+
+countNestedFunctionLevel =
+    #
+    # Given a Function object, this walks up the AST 
+    #
+    #
+function(f, count = 0)
+{
+    f2 = asFunction(f$parent)
+    if(!is.null(f2))
+        return(getNested(f2, count + 1))
+    count
+}
+
+if(FALSE) {
+  p = to_ast(parse("~/R-devel3/src/library/tools/R/QC.R"))
+  s3 = findS3ClassDefs(p)
+
+  inh = find_nodes(p, function(x) is(x, "Call" ) && is_symbol(x$fn, "inherits"))
+}
+
+if(FALSE) {
+xmlFiles = getRFiles("~/GitWorkingArea/XML/R")
+asts = lapply(xmlFiles, function(f) to_ast(parse(f))); names(asts) = basename(xmlFiles)
+
+xml.s3 = lapply(asts, findS3ClassDefs)
+xml.s3 = xml.s3[sapply(xml.s3, length) != 0]
+
+xml.inh = lapply(asts, function(p) find_nodes(p, function(x) is(x, "Call" ) && (is_symbol(x$fn, "inherits") || is_symbol(x$fn, "is"))))
+names(xml.inh) = names(asts)
+xml.inh = xml.inh[sapply(xml.inh, length) > 0]
+}
+
+
+
+
+
+nonLocalAssigns =
+    #  nla = nonLocalAssigns(tools:::.check_packages, varName = TRUE)
+    # Note how we make this function more flexible by adding ...
+    # and passing that to w$ans(...)
+    # And that passes it to assignName.
+    # This allows the caller to control deparse, varName or default for assignName
+    # Also note how we moved the code in assignName into its own function
+    # from an anonymous function in the structure( names = sapply(, function))
+    # so it could be reused and called independently by the user.
+    # Then we made it more intelligent, adding the options
+    # and also making it take different types of language objects.
+    #
+function(code, w = mkNLAWalker(), ...)
+{
+    codetools::walkCode(code, w)
+    w$ans(...)
+}
+
+mkNLAWalker =
+function()
+{
+    ans = list()
+    
+    leaf = function(x, w) {
+        ty = typeof(x)
+        if(ty == "pairlist") {
+            lapply(x, walkCode, w)
+            return(NULL)
+        } else if(ty == "closure") {
+            walkCode(formals(x), w) # lapply(formals(x), walkCode, w)
+            walkCode(body(x), w)
+        }
+
+        NULL
+    }
+
+    call = function(x, w) {
+        if(is.name(x[[1]]) && as.character(x[[1]])== "<<-")
+            ans[[ length(ans) + 1L]] <<- x
+        
+        for (ee in as.list(x))
+            if (!missing(ee))
+                walkCode(ee, w)
+    }
+
+    list(handler = function(...) NULL,
+         call = call,
+         leaf = leaf,
+         ans = function(...) {
+                 structure(ans, names = sapply(ans, assignName, ...))
+             })
+}
+
+assignName =
+function(x, deparse = FALSE, varName = FALSE)
+{
+    if(is.name(x[[2]]))
+       as.character(x[[2]])
+    else {
+        if(varName)
+            switch(class(x),
+                   "<-"=,
+                   "=" = assignName(x[[2]], varName = TRUE),
+                   call = switch(as.character(x[[1]]),
+                                  "["=,
+                                  "[["=,
+                                  "<<-"=,                   
+                                 "$" = assignName(x[[2]], varName = TRUE),
+                       deparse(x[[2]])),
+                   "??"
+                   )
+        else if(deparse)
+            paste(deparse(x), collapse = "")
+        else
+            ""
+        }
+}
