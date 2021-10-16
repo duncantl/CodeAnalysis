caa2daf7b1591a01f96324e601ad04148eb5eacb
Duncan Temple Lang <duncan@r-project.org>
2021-10-16 16:20:41 -0700

    remove browser() cal

--- R/callGraph.R
+++ R/callGraph.R
@@ -46,7 +46,7 @@ setMethod("callGraph", "FunctionsByFile",
               ans = callGraph(structure(unlist(obj), names = unlist(lapply(obj, names)))) # callNextMethod() #  withinPackage, ...)
               fnFileMap = structure(rep(basename(names(obj)), sapply(obj, length)),
                                     names = unlist(lapply(obj, names)))
-browser()              
+
               ans$callerFile = fnFileMap[ ans[, 1] ]
               ans$calledFile = fnFileMap[ ans[, 2] ]
               ans
