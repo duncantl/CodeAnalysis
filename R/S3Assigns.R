a515f5e64d60768601297643486508eeebf3b8da
Duncan Temple Lang <duncan@r-project.org>
2021-12-13 06:38:22 -0800

    avoid errors when fewer elements in expression than expected

--- R/S3Assigns.R
+++ R/S3Assigns.R
@@ -1,77 +1,3 @@
-if(FALSE) {
-    # Older, slower useful reference implementation.
-S3Assignments =
-    #
-    #
-    #
-function(fun, asNode = FALSE)
-{
-  fun = to_ast(fun)
-  i = find_nodes(fun, isS3Assign)
-  if(length(i)) {
-      if(asNode)
-          i
-      else
-          return(lapply(i, function(node) extractS3Class(node)))
-  } else
-     NULL
-}
-
-isS3Assign =
-function(n)      
-{
-  (is(n, "Call") && is(n$fn, "Symbol") && n$fn$value == "class<-") ||
-      (is(n, "Call") && is(n$fn, "Symbol") && n$fn$value == "structure" && "class" %in% names(n$args$contents))
-}
-
-extractS3Class =
-    # node is typically going to be a Call to
-    #  class<- "val"
-    #  class <- c("val1", "val2")
-    #  class <- c("val1", sym)    
-    #  class <- foo(...)
-    #  structure(..., class = "value")
-    #  structure(..., class = c("value", "value2"))
-    #  structure(..., class = c("value", variable))
-    #  structure(..., class = foo("value", variable))    
-    #
-function(node)
-{
-    if(node$fn$value == "structure") {
-        #XXX node$args[["class"]]  doesn't work. Fix rstatic.
-        i = match("class", names(node$args))
-        if(is.na(i)) {
-            warning("problems with S3 class assignment")
-            return(character())
-        }
-        tmp = node$args$contents[[i]]
-        if(is(tmp, "Call"))
-            sapply(tmp$args$contents, function(x) if(is(x, "Character")) x$value else NA)
-        else if(is(tmp, "Character"))
-            tmp$value
-        else
-            NA
-    } else {
-        # call to class<-() so right hand size.
-        # Could be  string, c(str, str), foo(...)
-        args = node$args[[2]]
-        if(is(args, "Call"))
-            sapply(args$args$contents, function(x) if(is(x, "Character")) x$value else NA)
-        else if(is(args, "Character"))
-            args$value
-        else if(is(args, "Symbol"))
-            NA
-        else if(is(args, "Null"))
-            NULL
-        else {
-            warning("don't yet recognize this class assignment")
-            args
-        }
-    }
-}
-} # end of if(FALSE)
-
-
 #################################################
 
 S3Assignments =
@@ -109,7 +35,8 @@ function(recursive = TRUE)
     
     call = function(x, w) {
         if(isS3Assign(x))  #  && !is.na((val <- extractS3Class2(x[[3]]))))
-            defs[[ length(defs) + 1L ]] <<- extractS3Class(x[[3]])
+            defs[[ length(defs) + 1L ]] <<- extractS3Class(if(length(x) >= 3) x[[3]] else x[[2]])
+                                                # structure(class = "aat_splithalf") only has 2 elements.  In AATools
         
         for (ee in as.list(x))
             if (!missing(ee))
