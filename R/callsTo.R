b41230cb9eda06f675683aca20e4f59d26310ef9
Duncan Temple Lang <duncan@r-project.org>
2022-02-05 17:18:13 -0800

    consolidated the code

--- R/callsTo.R
+++ R/callsTo.R
@@ -19,11 +19,10 @@ function(funNames)
 
     call = function(x, w) {
         isName = is.name(x[[1]])
-        if(isName && (length(funNames) == 0 || as.character(x[[1]]) %in% funNames)) {
+        if((isName && (length(funNames) == 0 || as.character(x[[1]]) %in% funNames)) ||
+               ( is.call(x[[1]]) && is.name(x[[1]][[1]]) && as.character(x[[1]][[1]]) %in% c("::", ":::") && deparse(x[[1]]) %in% funNames ) ) {
             calls[[length(calls) + 1L]] <<- x
-        } else if(is.call(x[[1]]) && is.name(x[[1]][[1]]) && as.character(x[[1]][[1]]) %in% c("::", ":::") && deparse(x[[1]]) %in% funNames) {
-                calls[[length(calls) + 1L]] <<- x
-        }
+        } 
         
 
         els = as.list(x)
