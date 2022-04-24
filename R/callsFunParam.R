edacc1d0f570c897197eb32f7811e112561d0322
Duncan Temple Lang <duncan@r-project.org>
2022-04-24 11:12:38 -0700

    comments about this being done elsewhere

--- R/callsFunParam.R
+++ R/callsFunParam.R
@@ -1,11 +1,14 @@
 
+# I think I already implemented this. Look in the R files.
+#  findCallsParam ???
+# # !!! Look in freeVariables.R  - usedAsFunction and isParamUsedAsFun
 #
 # The idea here is to find functions which take a parameter
 # and then call/invoke that parameter as a function, e.g. lapply, mapply, curve, optim, do.call, outer
-# RCIndex::parseTU() and visitTU() (although that hits C code)
+# RCIndex::parseTU() and visitTU() (although that hits C code). Or Rllvm.
 #
-# I think I implemented this. Look in the R files.
-#  findCallsParam ???
+#
+#
 #
 if(FALSE) {
 
@@ -31,7 +34,7 @@ findCallsParam =
     # Doesn't find indirect calls such as function(x, f) sapply(x, f).
     # Get those with getGlobals()
     #
-    # Does't detect calls from C code of course. See the RCIndex package 
+    # Doesn't detect calls from C code of course. See the RCIndex package 
     # and NativeCodeAnalysis package for that..
     #
 function(fun, asCalls = FALSE)
