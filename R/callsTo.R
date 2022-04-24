f162c4c5d43cc3356f9e5579a6c7dab440b303ca
Duncan Temple Lang <duncan@r-project.org>
2022-04-24 11:08:32 -0700

    added some tweaks and also handle the result of a call to getAnywhere() which may have multiple objects.  Use the first.

--- R/callsTo.R
+++ R/callsTo.R
@@ -7,7 +7,7 @@ function(funNames)
 
     leaf = function(x, w, ...) {
         ty = typeof(x)
-        if(ty == "pairlist") {
+        if(ty == "pairlist" || ty == "expression") {
             lapply(x, walkCode, w)
             return(NULL)
         } else if(ty == "closure") {
@@ -18,21 +18,24 @@ function(funNames)
     }
 
     call = function(x, w) {
+
         isName = is.name(x[[1]])
         if((isName && (length(funNames) == 0 || as.character(x[[1]]) %in% funNames)) ||
                ( is.call(x[[1]]) && is.name(x[[1]][[1]]) && as.character(x[[1]][[1]]) %in% c("::", ":::") && deparse(x[[1]]) %in% funNames ) ) {
             calls[[length(calls) + 1L]] <<- x
         } 
         
-
+        
         els = as.list(x)
         if(isName && as.character(x[[1]]) %in% c(".Internal", ".Primitive")) 
             els = els[-2]
 
         
-        for (ee in els)
+        for (ee in els) {
             if (!missing(ee))
                 walkCode(ee, w)
+        }
+        
     }    
 
     list(handler = function(x, w) NULL, leaf = leaf, call = call, ans = function() calls )
@@ -41,6 +44,12 @@ function(funNames)
 findCallsTo =
 function(code, funNames = character(), walker = mkCallWalker(funNames))
 {
+    if(is(code, "getAnywhere")) {
+        if(length(code$objs) > 1)
+            stop("more than one function in the getAnywhere object")
+        code = code$objs[[1]]
+    }
+    
     walkCode(code, walker)
     walker$ans()
 }
