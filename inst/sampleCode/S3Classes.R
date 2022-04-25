8f04e769350c40efc18b89d0b18ac6eef6078878
Duncan Temple Lang <duncan@r-project.org>
2022-04-25 09:38:45 -0700

    sample files

---
+++ inst/sampleCode/S3Classes.R
@@ -0,0 +1,55 @@
+f1 =
+function(x)
+{
+    ans = c(low = min(x), high = max(x))
+    class(ans) = c("Extent")
+    ans
+}
+
+f2 =
+function(x)
+{
+    ans = c(low = min(x), high = max(x))
+    class(ans) = "Extent"
+    ans
+}
+
+
+f3 =
+function(x)
+{
+    ans = c(low = min(x), high = max(x))
+    class(ans) = c("Extent", "1D")
+    ans
+}
+
+
+f3.5 =
+function(x)
+{
+    sym = "1D"
+    ans = c(low = min(x), high = max(x))
+    class(ans) = c("Extent", sym)
+    ans
+}
+
+
+f4 =
+function(x)
+    structure(c(low = min(x), high = max(x)), class = c("Extent", "1D"))
+
+f4.5 =
+function(x) {
+    sym = "1D"       
+    structure(c(low = min(x), high = max(x)), class = c("Extent", sym))
+}
+
+
+f5 =
+function(x)
+{
+    sym = "Bob"
+    ans = c(low = min(x), high = max(x))
+    class(ans) = c("Extent", sym)
+    ans
+}
---
+++ inst/sampleCode/ifScript.R
@@ -0,0 +1,28 @@
+
+cond = TRUE
+x = 1
+y = 2
+N = 1000
+
+if(TRUE) {
+    z = 3
+    foo(x, z)
+}
+
+d = rnorm(N)
+hist(d)
+rug(d)
+
+if(FALSE) {
+    z = 101
+    bar(y, z)
+}
+
+plot(1:10)
+
+if(cond) {
+
+}
+
+
+
