9026f6dda751780826879346acb0521f4c1df58d
Duncan Temple Lang <duncan@r-project.org>
2023-08-23 09:12:35 -0700

    look in the .__NAMESPACE__. imports environment

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,7 @@
 pkgSymbolsUsed =
     #
+    #  Doesn't the tools package have a function that does this???
+    #
     #  Find the symbols used in functions in package pkg
     #  that come from the package fromPkg. This is useful
     #  for refining the import() command in NAMESPACE to
@@ -8,15 +10,44 @@ pkgSymbolsUsed =
     #
     #  symbolsUsed("Rllvm", "Rffi")
     #
-function(pkg, fromPkg, ns = getNamespace(pkg))
+    #  XXX need to differentiate/identify from which package a particular
+    #  function is being imported.
+    #   If pkg A imports packages X and Y and both X and Y have a symbol foo,
+    #   which one does A import  and can we get the provenance.
+    #
+    #  This is probably not getting the methods we import or the operators, e.g
+    #  RAutoGenRunTime's & and  |.
+    #
+function(pkg, fromPkg, ns = getNamespace(pkg), exportedOnly = TRUE)
 {
     vals = as.list(ns, all = TRUE)
     isfun = sapply(vals, is.function)
     funs = vals[isfun]
-    g = lapply(funs, codetools::findGlobals)
+    g = lapply(funs, codetools::findGlobals) # should we use getGlobals() from this package.
     g = unique(unlist(g))
     o = setdiff(g, names(vals))
 
     vals2 = getNamespace(fromPkg)
-    o[ o %in% names(vals2) ] # might be in other packages also.
+
+
+    imports = ns$".__NAMESPACE__."$imports
+    contains = function(x, val)  val %in% x
+
+    # Do we need  to reverse the order of imports. base is first.
+    from = sapply(o, function(var) names(imports)[which(sapply(imports, contains, var))[1]])
+    names(from) = o
+    w = !is.na(from)
+    from = from[w]
+    # Find the values which are from  fromPkg
+    ans = names(from)[from == fromPkg]
+    return(ans)
+
+    #XXX
+    if(exportedOnly) {
+       exports = ls(vals2$".__NAMESPACE__."$exports, all.names = TRUE)
+       o[ o %in% exports ]
+   } else {
+        fromPkg.imports = ns$".__NAMESPACE__."$imports[[fromPkg]]    
+        o[ o %in% names(vals2) ] # might be in other packages also.
+    }
 }
