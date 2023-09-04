7adf562b4f8d4a7e9e9602dcab9b5dac87239842
Duncan Temple Lang <duncan@r-project.org>
2023-09-03 18:30:05 -0700

    made findCallsTo() accept not simple strings but R calls/expressions and then compare those. This allows us to match calls to x(a, b) by specifying x.  This will automatically parse the funNames if not all of them are is.name().

--- R/callsTo.R
+++ R/callsTo.R
@@ -1,6 +1,7 @@
 # find the calls to given functins
 
 mkCallWalker =
+    #
 function(funNames)
 {
     calls = list()
@@ -17,11 +18,22 @@ function(funNames)
         } 
     }
 
+
+    isFunNamesStrings = is.character(funNames)
+    ok = function(x, isName, isFunNamesStrings) {
+            if(!isFunNamesStrings) 
+                return(any(sapply(funNames, identical, x[[1]])))
+             
+           (isName && (as.character(x[[1]]) %in% funNames)) ||
+               ( is.call(x[[1]]) && is.name(x[[1]][[1]]) &&
+                   as.character(x[[1]][[1]]) %in% c("::", ":::") && deparse(x[[1]]) %in% funNames ) 
+          }
+
+    
     call = function(x, w) {
 
         isName = is.name(x[[1]])
-        if((isName && (length(funNames) == 0 || as.character(x[[1]]) %in% funNames)) ||
-               ( is.call(x[[1]]) && is.name(x[[1]][[1]]) && as.character(x[[1]][[1]]) %in% c("::", ":::") && deparse(x[[1]]) %in% funNames ) ) {
+        if(length(funNames) == 0 || ok(x, isName, isFunNamesStrings)) {
             calls[[length(calls) + 1L]] <<- x
         } 
         
@@ -43,14 +55,24 @@ function(funNames)
 
 findCallsTo =
     # How is this related to findCallsToFunctions()?
-function(code, funNames = character(), walker = mkCallWalker(funNames))
+    #
+    #
+    # f = getAnywhere("is.nloptr")$objs[[1]]
+    # findCallsTo(f, list(parse(text = "x$eval_f")[[1]]))
+    # findCallsTo(f, "x$eval_f")
+    # findCallsTo(f, "x$eval_f", parse = TRUE)
+    #
+function(code, funNames = character(), walker = mkCallWalker(funNames), parse = any(!sapply(funNames, is.name)))
 {
+
+    if(parse) 
+        funNames = lapply(funNames, function(x) parse(text = x)[[1]])
+    
     if(is(code, "getAnywhere")) {
         if(length(code$objs) > 1)
             stop("more than one function in the getAnywhere object")
         code = code$objs[[1]]
     }
-    
     
     walkCode(code, walker)
     walker$ans()
