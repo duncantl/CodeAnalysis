9a43e6cc433ebf855104a0a4bee3fe375f1269ca
Duncan Temple Lang <duncan@r-project.org>
2023-11-22 20:40:12 -0800

    export numCalls and getPkgFunctions

---
+++ R/RfuncSizes.R
@@ -0,0 +1,22 @@
+# This file came from ~/Book/HackingREngine/Bugs/trace/RfuncSizes.R
+# with the purpose of computing a data.frame of the number of calls
+# in each function in each R package.
+#
+
+numCalls =
+function(fun)
+{
+    length(findCallsTo(fun))
+}
+
+getPkgFunctions =
+    # Does getFunctionDefinitions() do this already.
+function(pkg)
+{
+    if(!require(pkg, character.only = TRUE))
+        return(list())
+    objs = as.list.environment(getNamespace(pkg), TRUE)
+    objs[ sapply(objs, is.function) ]
+}
+
+
