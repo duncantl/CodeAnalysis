f24bf33ce13859c68fcb32d718a8be7997319e4e
Duncan Temple Lang <duncan@r-project.org>
2023-11-25 22:28:29 -0800

    examples of unused assignments

--- tests/unusedAssigns.R
+++ tests/unusedAssigns.R
@@ -9,7 +9,32 @@ function()
   d = 10
   a + c
 }
+f =
+function()
+{
+  a = 1
+  b = 2
+  c = 3
+  d = 10
+  a + c
+}
 
 findUnusedAssignments(f)
+
+f2 = function(x)
+{
+    a = 1
+    if(x > 3) {
+        b = foo(2)
+        a = a + b
+    }
+
+    a
+}
+
+x = findUnusedAssignments(f2, FALSE, FALSE)
+# WRONG:  thinks b is not necessary but it is.
+
+
 
 
