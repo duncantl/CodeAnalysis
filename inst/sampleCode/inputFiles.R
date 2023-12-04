6ccce7e06dbab0047569835311fa4cbed36a3c1d
Duncan Temple Lang <duncan@r-project.org>
2023-12-03 19:59:46 -0800

    utils::write..  MASS::write.matrix and graphics device calls

--- inst/sampleCode/inputFiles.R
+++ inst/sampleCode/inputFiles.R
@@ -8,5 +8,18 @@ readRDS("results.rds")
 saveRDS(d2, "today.rds")
 write.csv(d2, "today.csv")
 
+utils::write.table(d2, "foo.csv")
+
+MASS::write.matrix(mat, "myMatrix.mat")
+
+
+png("foo.pdf", 7, 8)
+dev.off()
+
+jpeg("foo.jpg", 1000, 1000)
+dev.off()
+
+png("foo.jpg", 1000, 1000)
+dev.off()
 
 
