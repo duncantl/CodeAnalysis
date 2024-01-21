1cd8433941638a242b05d048034f2dc6ab6651a5
Duncan Temple Lang <duncan@r-project.org>
2024-01-21 08:44:28 -0800

    notes/observations

--- explorations/VarietyTrial.R
+++ explorations/VarietyTrial.R
@@ -8,12 +8,13 @@ src = getSourceInfo(dir)
 src2 = src[ !is.na(src$sourced), ]
 src2$from = gsub(paste0("^", path.expand(dir), "/"), "", src2$from)
 g = graph_from_data_frame(src2)
-V(g)$color[ grepl("^(~|/)", V(g)$name) ] = "blue"
+V(g)$color = "salmon"
+V(g)$color[ grepl("^(~|/)", V(g)$name) ] = "green"
 plot(g)
 
-plot(g, layout = layout_as_tree)
-plot(g, layout = layout_with_fr)
-plot(g, layout = layout_in_circle)
+# plot(g, layout = layout_as_tree)
+# plot(g, layout = layout_with_fr)
+# plot(g, layout = layout_in_circle)
 
 #
 # read_xl_data.R has source("code/funs.R").
@@ -27,6 +28,36 @@ plot(g, layout = layout_in_circle)
 # src[ !is.na(src$sourced) & src$sourced == "code/funs.R", ]
 #
 #
+
+# Can see
+# prep_stan_data.R ->  prep_col_score.R -> funs.R
+# prep_stan_data.R -> funs.R
+# and prep_col_score.R is not sourced by any other file.
+# So prep_col_score.R doesn't need to source("funs.R")
+# unless the order of the commands makes this necessary.
+#
+# Similarly,
+#  prep_stan_data.R -> prep_temp_data.R -> est_PI.R -> funs.R
+# and prep_stan_data.R also source()'s funs.R
+#
+# 3 disconnected sub graphs
+#  plot_weather.R -> ~/vt_project/code/funs.R
+#  qc_weather.R -> qc_weather_funs.R
+#  retrieve_ucipm.R ->  [ getCIMIS.R & uc_ipm_funs.R ]
+#
+# Do these scripts depend on variables created in other files.
+
+gv = lapply(file.path(dir, c("plot_weather.R", "qc_weather.R", "retreive_ucipm.R")), freeVariables)
+
+v1 = freeVariables("/Users/duncan/Book/ExploreCode/Variety_trial_analysis/code/plot_weather.R")
+# so
+#  plot_weather: "site_weather"
+#  qc_weather : "all_weather" "all_info"   
+#  retreive_ucipm.R: "xmlValue"
+#     And freeVariables doesn't know that xmlValue is availale via the library(XML) call
+#     Also doesn't know about xpathSApply() as being like lapply() and calling its FUN argument.
+
+
 
 ###############
 
