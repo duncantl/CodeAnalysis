2bbe22a11d579700b50b99fb8756b7345ce8b620
Duncan Temple Lang <duncan@r-project.org>
2025-02-18 11:03:21 -0800

    tests for getGlobals() for substitute(), bquote()

---
+++ tests/substitute.R
@@ -0,0 +1,59 @@
+s1 = 
+function(a, b)
+{
+    substitute(a + val, list(val = a + b))
+}
+
+s2 =
+function(a, b)
+{
+    substitute(a + val, list(val = a + x))
+}
+
+s3 =
+function(a, b)
+{
+    x = 1
+    substitute(a + val, list(val = a + x))
+}
+
+tmp = lapply(list(s1, s2, s3), function(x) getGlobals(x)$variables)
+stopifnot(tmp[[2]] == "x")
+stopifnot(all(sapply(tmp[c(1, 3)], length) == 0))
+
+
+b1 =
+function(x)    
+{
+    bquote(x == x)
+    bquote(x == .(x))
+}
+
+b2 =
+function(x)    
+{
+    bquote(x == .(x + a))
+}
+
+
+b3 =
+function(x, a)    
+{
+    bquote(x == .(x + a))
+}
+
+b4 =
+function(x, env)    
+{
+    bquote(x == .(x + a), env)
+}
+o = getGlobals(b4)
+stopifnot(length(o$variables) == 0)
+
+b5 =
+function(x, env0)    
+{
+    bquote(x == .(x + a), env, splice = doSplice)
+}
+o = getGlobals(b5)
+stopifnot(all(o$variables == c("env", "doSplice")))
