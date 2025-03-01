757e25e44f2a14704e785b279d23f5e7d873cb5a
Duncan Temple Lang <duncan@r-project.org>
2025-03-01 12:35:04 -0800

    getScriptGlobal takes either the name of the file to parse or the already parsed code

--- R/getScriptGlobals.R
+++ R/getScriptGlobals.R
@@ -1,7 +1,11 @@
 getScriptGlobals =
 function(file, ...)
 {
-    e = parse(file)
+    if(is.language(file))
+        e = file
+    else
+        e = parse(file)
+    
     f = function() {}
     body(f)[seq(along = e) + 1] = e
     getGlobals(f, ...)
