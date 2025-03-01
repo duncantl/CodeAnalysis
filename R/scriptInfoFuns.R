8fdff2d4ab74e12ae84c00af040176e1fceca10e
Duncan Temple Lang <duncan@r-project.org>
2025-03-01 12:09:20 -0800

    functions to analyze a set of scripts and a) find scripts that reference variables that aren't previously defined in that script, and b) find those variables definitions/initializations in other scripts to indicate which scripts may have to be run first.  This intentionally  currently only looks for simple assignments.

---
+++ R/scriptInfoFuns.R
@@ -0,0 +1,59 @@
+missingVars = 
+function(script, dir = ".", allScripts = list.files(dir, pattern = "\\.R$", full.names = TRUE),
+          varDefs = varDefsByFile(, allScripts))
+{
+#   vars = lapply(allScripts, topLevelAssignedVars)
+#   names(vars) = allScripts
+#   varDefs = varDefsByFile(vars)
+    
+    ans = lapply(script,
+                 function(file)
+                     findMissingVariables(unique(getScriptGlobals(file)$variables), varDefs))
+
+    if(length(script) == 1)
+        return(ans[[1]])
+
+    
+    names(ans) = script
+    ans
+}
+
+         
+
+topLevelAssignedVars =
+function(x, unique = TRUE)
+{
+    if(is.character(x)) {
+        if(file.exists(x))
+            x = parse(x)
+        else
+            x = parse(text = x)
+    }
+    
+    x = x[sapply(x, isSimpleAssignTo)]
+    ans = sapply(x, function(x) deparse(x[[2]]))
+    if(unique)
+        unique(ans)
+    else
+        ans
+}
+
+
+findMissingVariables =
+function(miss, defs)    
+{
+   miss = unique(miss)
+   structure(lapply(miss, function(v) defs$file[v == defs$variable]), names = miss)
+}
+
+varDefsByFile =
+function(vars, files = names(vars))
+{
+    if(missing(vars))
+       vars = lapply(files, topLevelAssignedVars)
+    
+    data.frame(variable = unlist(vars),
+               file = rep(files, sapply(vars, length)),
+               row.names = NULL
+               )
+}
