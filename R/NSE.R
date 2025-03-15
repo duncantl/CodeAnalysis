ca330850bc40acc82bf74054d27a2fe3c523378a
Duncan Temple Lang <duncan@r-project.org>
2025-03-15 15:02:17 -0700

    findNSE() to use findCallsTo

--- R/NSE.R
+++ R/NSE.R
@@ -1,20 +1,11 @@
 
-
-findNSE =
-function(code, asNodes = TRUE)
-{
-    code = to_ast(code)
-    find_nodes(code, isNSE)
-#  if(length(idx))
-#      lapply(idx, function(i) code[[i]])
-#  else
-#      list()
-}
-
 NSEFunNames = c("eval", "evalq", "get", "assign", "rm", "eval.parent", "exists")
 
-isNSE =
-function(node, nseFunNames = NSEFunNames)
+findNSE =
+function(code, asNodes = TRUE, nseFunNames = NSEFunNames)
 {
-    is(node, "Call") && is(node$fn, "Symbol") && node$fn$value %in% nseFunNames
+    findCallsTo(code, nseFunNames)
 }
+
+
+
