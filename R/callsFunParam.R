99697e0631a9e752c091dc8ec46a61c92c113f2d
Duncan Temple Lang <duncan@r-project.org>
2025-03-15 15:08:05 -0700

    switch findCallsParam() to use findCallsTo()

--- R/callsFunParam.R
+++ R/callsFunParam.R
@@ -18,7 +18,12 @@ if(FALSE) {
   foo1 = function(files, f) {
              tmp = lapply(files, f)
              do.call(rbind, tmp)
-         }
+  }
+
+  foo3 = function(files, f, g) {
+             tmp = lapply(files, f)
+             do.call(g, tmp)
+         }    
 
 
   foo2 = function(x,f)    
@@ -31,20 +36,23 @@ findCallsParam =
     # Find any direct calls to a parameter, e.g.
     #   optim() may call its fn and gr
     #
-    # Doesn't find indirect calls such as function(x, f) sapply(x, f).
-    # Get those with getGlobals()
+    # ? Doesn't find indirect calls such as function(x, f) sapply(x, f).
+    # Get those with getGlobals().
+    # Actually, findCallsTo does find use in lapply()
     #
     # Doesn't detect calls from C code of course. See the RCIndex package 
     # and NativeCodeAnalysis package for that..
     #
 function(fun, asCalls = FALSE)
 {
-   params = names(formals(fun))
-   funa = to_ast(fun)
-   v = find_nodes(funa, function(x) is(x, "Call") && is(x$fn, "Symbol") && x$fn$value %in% params)
-   if(asCalls)
-       v
-   else
-      unique(vapply(v, function(x) x$fn$value, character(1)))
+    params = names(formals(fun))
+
+    v = findCallsTo(fun, params)
+
+    if(asCalls)
+        v
+    else
+          # should we use deparse rather than as.character(). Shouldn't matter as direct call to parameter name.
+       unique(vapply(v, function(x) as.character( x[[1]] ), character(1)))
 }
 
