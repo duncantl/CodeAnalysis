18e47a15816e4e13be242893299ef52b16953a50
Duncan Temple Lang <duncan@r-project.org>
2025-03-15 14:47:27 -0700

    findUsedOptions via rlanguage objects and walkCode.

--- R/options.R
+++ R/options.R
@@ -1,26 +1,79 @@
 #
 #
 
+if(FALSE) {
+    findOptionsUses(quote(options()["abc"]))
+    findOptionsUses(quote(options()[c("abc", "def")]))
+    findOptionsUses(quote(options()[c("abc", x)]))        
+    findOptionsUses(quote(options()[x]))
+    findOptionsUses(quote(options()[["abc"]]))    
+    findOptionsUses(quote(options()$abc))
+    findOptionsUses(quote(options()$"abc"))        
+}
+
+
+findOptionsUses =
+function(call, isName)    
+{
+
+    w = isCallTo(call, "options") && length(call) == 2 && is.character(call[[2]])
+    w = w | isCallTo(call, "getOption")
+    if(w)
+        return(TRUE)
+    
+    if(!isCallTo(call, c("[[", "$", "[")))
+        return(FALSE)
+
+    op = as.character(call[[1]])
+    isCallTo(call[[2]], "options") &&
+          ( isLiteral(call[[3]]) || (op == '$' && is.name(call[[3]] )))
+}
+
+
 findUsedOptions =
 function(code, asNodes = FALSE)
 {
-    code = to_ast(code)
-    idx = find_nodes(code, isUsedOption)
-    if(asNodes)
-        idx
-    else
-        sapply(idx, getUsedOptionName)
+#  k = findCallsTo(code, "getOption")
+#  w = sapply(k, function(x) length(x) > 1 && is.character(x[[2]]))
+#  optNames1 = sapply(k[w], `[[`, 2)
+
+    k2 = findCallsTo(code, walker = mkCallWalkerPred(findOptionsUses))
+    sapply(k2, getUsedOptionName)
+#    c(optNames1, optNames2)
 }
 
-
-isUsedOption =
-function(node)
+getUsedOptionName =
+function(x)
 {
-    is(node, "Call") && is(node$fn, "Symbol") && ((node$fn$value == "getOption" || (node$fn$value == "options" && length(node$args) > 0)) ) ||
-        (node$fn$value %in% c("$", "[[", "[") && length(node$args$contents) > 0 && is(node$args$contents[[1]], "Call") && is(node$args$contents[[1]]$fn, "Symbol") && node$args$contents[[1]]$fn$value == "options")
+    if(isCallTo(x, c("options", "getOption")))
+        return(x[[2]])
+
+    # Otherwise, call of form options()[[ ]] or $ or [ with literals in the subsetting.
+    ans = x[[3]]
+    if(is.call(ans)) 
+        return( as.character(ans[-1]) )
+
+    ans
 }
 
 
+if(FALSE) {
+    k = findCallsTo(code, "getOption")
+    w = sapply(k, function(x) length(x) > 1 && is.character(x[[2]]))
+    optNames1 = sapply(k[w], `[[`, 2)    
+
+    nargs = sapply(k, length) - 1
+    w = (sapply(k, isCallTo, "getOption") & nargs > 0) | (sapply(k, isCallTo, "options") & nargs > 0) 
+    k = k[w]
+    
+    if(asNodes)
+        k
+    else
+        sapply(k, getUsedOptionName)
+}
+
+
+if(FALSE) #<<<<<<<<<<
 getUsedOptionName =
 function(node)
 {
