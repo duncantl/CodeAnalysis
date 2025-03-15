64e45e7768d65f8458c7be1f0878bdcf3b8eb24b
Duncan Temple Lang <duncan@r-project.org>
2025-03-15 16:10:15 -0700

    examples for usesDots()

---
+++ tests/dots.R
@@ -0,0 +1,21 @@
+bar = 
+function(x, y = rep(1, length(x)), ...)
+{
+    a = list(...)
+    plot(x, y, ...)    
+    if(a[1] > 2)
+        text(x, y, ...)
+        
+}
+
+
+d = usesDots(bar)
+stopifnot(length(d) == 4)
+
+f =
+function(x, ...)    
+{
+    x = list(...)
+    bar(x, ...)
+    bar(x, list(...))
+}
