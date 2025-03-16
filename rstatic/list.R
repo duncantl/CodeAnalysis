29f4cbd471c3edf611addcef2ed11a7e116ddeef
Duncan Temple Lang <duncan@r-project.org>
2025-03-16 14:25:33 -0700

    code to read ToChange.md and give summary of what functions are remaining to be addressed

---
+++ rstatic/list.R
@@ -0,0 +1,17 @@
+ll = readLines("~/GitWorkingArea/CodeAnalysisWORstatic/ToChange.md")
+ll = ll[ll != ""]
+s = split(ll, cumsum(grepl("^#", ll)))
+names(s) = gsub("# ", "", sapply(s, `[`, 1))
+sapply(s[2:5], function(x) length(grep("^\\+", x)))
+
+f = sapply(s[2:5], function(x) gsub("^\\+ ", "", grep("^\\+ *", x, value = TRUE)))
+
+u = unlist(f)
+u2 = gsub("^\\[[^]]+ ", "", u)
+w = duplicated(u2)
+table(w)
+
+# Show the current ones we haven't finished.
+sapply(f[2:5], function(x) grep("^[\\[√]", x, value = TRUE, invert = TRUE))
+
+
