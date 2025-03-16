13c949975a32d0bec360d6a41d9d052d644ab5f8
Duncan Temple Lang <duncan@r-project.org>
2025-03-16 13:52:40 -0700

    turned the example options calls into tests for findUsedOptions

--- tests/options.R
+++ tests/options.R
@@ -1,19 +1,46 @@
-o = options()
-o = options("digits")
-o = options()$digits
-o = options()["digits"]
-o = options()[["digits"]]
-o = options()[c("digits")]
-o = options()[c("digits", "warn")]
-options()[ c("abc", "xyz") ]
-options()[[ "abc" ]]
+library(CodeAnalysis)
 
-options(warn = 2)
-options(other = "2")
+tmp = findUsedOptions(quote( o <- options()))
+stopifnot(length(tmp) == 0)
 
-options(x, "foo") <- 1
+tmp <- findUsedOptions(quote( o <- options("digits")))
+stopifnot(tmp == "digits")
 
-getOption("warn")
-getOption("warn", NA)
+
+tmp <- findUsedOptions(quote(o <- options()$digits))
+stopifnot(tmp == "digits")
+
+tmp <- findUsedOptions(quote(o <- options()["digits"]))
+stopifnot(tmp == "digits")
+
+tmp <- findUsedOptions(quote(o <- options()[["digits"]]))
+stopifnot(tmp == "digits")
+
+tmp <- findUsedOptions(quote(o <- options()[c("digits")]))
+stopifnot(tmp == "digits")
+
+tmp <- findUsedOptions(quote(o <- options()[c("digits", "warn")]))
+stopifnot(all(tmp == c("digits", "warn")))
+
+tmp <- findUsedOptions(quote(options()[ c("abc", "xyz") ]))
+stopifnot(all(tmp == c("abc", "xyz")))
+
+tmp <- findUsedOptions(quote(options()[[ "abc" ]]))
+stopifnot(tmp == "abc")
+
+tmp <- findUsedOptions(quote(options(warn = 2)))
+stopifnot(length(tmp) == 0)
+
+tmp <- findUsedOptions(quote(options(other = "2")))
+stopifnot(length(tmp) == 0)
+
+tmp <- findUsedOptions(quote(options()[[x]]))
+
+tmp <- findUsedOptions(quote(getOption("warn")))
+stopifnot(tmp == "warn")
+
+tmp <- findUsedOptions(quote(getOption("warn", NA)))
+stopifnot(tmp == "warn")
+
 
 
