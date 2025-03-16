69924b26cfd5df770661d5c87f6567324104f8cf
Duncan Temple Lang <duncan@r-project.org>
2025-03-16 14:02:54 -0700

    just a comment to add a nested functions example.

--- explorations/globalsRewrite/example/simple.R
+++ explorations/globalsRewrite/example/simple.R
@@ -16,3 +16,6 @@ function(n, B = 999)
 {
   replicate(B, { a = f(rnorm(n)); mean(g(a))})
 }
+
+# Add example of nested functions within a top-level function
+# and how they access non-locals in the parent function(s).
