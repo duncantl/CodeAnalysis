a68806a91d84bb0c663ab7de6944c0f73cfd2d50
Duncan Temple Lang <duncan@r-project.org>
2025-03-17 15:47:46 -0700

    code to compute the extracted functions for all the functions in the tools package and then compare the number of terms in the original and the reduced + extracted to see why we were getting differences. This illustrated a few important bugs in the mkModifyCodeWalker() function re. formals and missing arguments in calls. There is still a difference due to double counting multiple levels of nested functions. This suggests adding a recursive = TRUE/FALSE parameter for extractFunctions()

---
+++ explorations/extractFuns/toolsExtract.R
@@ -0,0 +1,58 @@
+ns = getNamespace("tools")
+tfns = as.list(ns, all.names = TRUE)
+w = sapply(tfns, is.function)
+nfns = lapply(tfns[w], getFunctionDefs)
+ex = lapply(tfns[w], \(x) try(extractFunctions(x)))
+
+# Expect 1 error currently  FOO1 <- FOO2 <- function()...
+err = sapply(ex, inherits, 'try-error')
+w2 = which(w)[!err]
+
+d = data.frame(numTerms = sapply(tfns[w2], numTerms),
+               sizeReducedFunction = sapply(ex[!err], function(x) numTerms(x$new)),
+               numExtractedFunctions = sapply(ex[!err], function(x) length(x$nested)),
+               sizeExtractedFunctions = sapply(ex[!err], function(x) if(length(x$nested)) sum(sapply(x$nested, numTerms)) else 0L),
+               # working with nfns is not really relevant as this includes anonymous functions.
+               numFunctions = sapply(nfns[!err], length),               
+               sizeNestedFunctions = sapply(nfns[!err], function(x) if(length(x)) sum(sapply(x, numTerms)) else 0L)
+               )
+
+tmp = summary(with(subset(d, numFunctions == 0), numTerms - sizeExtractedFunctions))
+tmp = subset(d, numFunctions == 0)
+summary(tmp$sizeNestedFunctions)
+summary(tmp$sizeExtractedFunctions)
+
+
+d2 = subset(d, numExtractedFunctions > 0)
+
+d2$back = with(d2, sizeReducedFunction + sizeExtractedFunctions + numExtractedFunctions*2 )
+
+d2$diff = with(d2, numTerms - back)
+summary(d2$diff)
+
+w3 = d2$diff != 0
+table(w3)
+
+d3 = d2[w3]
+d3[order(d3$numTerms), ]
+
+# format.codocClasses &  fetchRdDB
+#                        numTerms sizeReducedFunction numExtractedFunctions sizeExtractedFunctions numFunctions sizeNestedFunctions  back  diff
+# format.codocClasses          70                   9                     3                     99            1                  57   114   -44
+# fetchRdDB                    88                  14                     2                     84            1                  70   102   -14
+
+v = "format.codocClasses"
+o = tfns[[v]]
+n = ex[[v]]$new
+stopifnot(identical(formals(o), formals(n)))
+ob = as.list(body(o))
+nb = as.list(body(n))
+length(ob)
+
+
+
+# Check for multiple nested named functions
+ids = rownames(d2)[w3]
+nn = lapply(ex[ids], function(x) sapply(x$nested, function(x) length(findNamedFunctions(x))))
+sapply(nn, max)
+# all have at least one nested name function.
