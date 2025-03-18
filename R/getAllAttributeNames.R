168bbe59f180158cf18f12261104c8828d184e1c
Duncan Temple Lang <duncan@r-project.org>
2025-03-18 12:27:54 -0700

    now preserve the order of the attributes as they appear in the code/AST by finding calls to both structure and attr

--- R/getAllAttributeNames.R
+++ R/getAllAttributeNames.R
@@ -4,25 +4,30 @@ if(FALSE) {
     tfns = as.list(ns, all.names = TRUE)
     tfns = tfns[sapply(tfns, is.function)]
 
-    ats = lapply(tfns, getAllAttributeNames)
+    ats = lapply(tfns, getAttributeNames)
     head(dsort(table(unlist(ats))), 20)
 }
 
-# The order is not guaranteed as we get the attributes in structure() calls first
+# The order was not guaranteed as we get the attributes in structure() calls first
 # and then attr() calls.
-# We could arrange this with findCallsTo(x, c("attr", "structure"))
-# and then process the calls differently.
+# We now arrange this with findCallsTo(x, c("attr", "structure"))
+# and then process the calls depending on if it is to attr or structure.
 
 getAttributeNames =
 function(x)    
 {
-    a = findCallsTo(x, "attr")
-    ats = sapply(a, function(x) if(is.character(x[[3]])) as.character(x[[3]]) else NA)
-    c(getAttributeNames.structure(x),
-      unname(ats[!is.na(ats)])
-     )
+    a = findCallsTo(x, c("attr", "structure"))
+    ats = lapply(a, function(x) {
+                           switch(as.character(x[[1]]),
+                                  attr = if(is.character(x[[3]])) as.character(x[[3]]) else NA,
+                                  structure = names(x)[names(x) != "" ],
+                                  NA)
+                    })
+    ats = unlist(ats)
+    ats[!is.na(ats)]
 }
 
+# Can leave but not exported and now not used by getAttributeNames() above.
 getAttributeNames.structure =
 function(x)
 {
