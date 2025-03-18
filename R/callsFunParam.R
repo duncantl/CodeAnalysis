e015a939bffebc839b6c5f77db47eb880c68456c
Duncan Temple Lang <duncan@r-project.org>
2025-03-18 12:11:35 -0700

    handle indirect calls to a parameter and ensure we get the name of the parameter not the function that indirectly calls the parameter. Use getIndirectCallFunList() and match.call()

--- R/callsFunParam.R
+++ R/callsFunParam.R
@@ -10,28 +10,11 @@
 #
 #
 #
-if(FALSE) {
-
-  foo = function(x, g, f)    
-           lapply(split(x, g), f)
-
-  foo1 = function(files, f) {
-             tmp = lapply(files, f)
-             do.call(rbind, tmp)
-  }
-
-  foo3 = function(files, f, g) {
-             tmp = lapply(files, f)
-             do.call(g, tmp)
-         }    
-
-
-  foo2 = function(x,f)    
-           f(x + 1) + 2
-}
 
 
 findCallsParam =
+    #
+    # How is this related to getCallParam()? (freeVariables.R)
     #
     # Find any direct calls to a parameter, e.g.
     #   optim() may call its fn and gr
@@ -40,15 +23,12 @@ findCallsParam =
     # Get those with getGlobals().
     # Actually, findCallsTo does find use in lapply()
     #
-    #XXX
-    #  But then we need to handle such results differently 
-    #
     # Doesn't detect calls from C code of course. See the RCIndex package 
     # and NativeCodeAnalysis package for that..
     #
     #
     #
-function(fun, asCalls = FALSE)
+function(fun, asCalls = FALSE, indirect = getIndirectCallFunList())
 {
     params = names(formals(fun))
 
@@ -56,10 +36,64 @@ function(fun, asCalls = FALSE)
 
     if(asCalls)
         v
-    else
-        # should we use deparse rather than as.character(). Shouldn't matter as direct call to parameter name.
-        # Fix for lapply(), etc. functions, i.e., indirect calls to fun.
-        # Then reenable test in tests/getGlobals.R
-       unique(vapply(v, function(x) as.character( x[[1]] ), character(1)))
+    else {
+        ans = sapply(v, getCalledParam, params, indirect)
+        unique(ans)
+    }
+}
+
+getCalledParam =
+    # should we use deparse rather than as.character(). Shouldn't matter as direct call to parameter name.
+    # Fixed to also handle indirect calls to parameter but return that parameter name as the parameter
+    # being called.    
+function(x, paramNames, indirect = getIndirectCallFunList())
+{
+    if(isCallTo(x, paramNames, indirect = character()))
+        return( as.character(x[[1]]) )
+
+    if(!isSymbol(x[[1]]))
+        #XXX
+        return(NA)
+
+    fn = as.character(x[[1]])
+
+    # index into the call, so 1 more than the argument.
+    # Should use match.call() to order the arguments in the call.
+    # Can use getIndirectCallFunList() to get the argument by name or position.
+    fn = as.character(x[[1]])
+    k = match.call(get(fn), x)
+    i = match(indirect[fn], names(k))
+
+    if(is.na(i)) {
+
+        # not include formals(), body(), match.fun(), but could as
+        # this indicates the param is expected to be a function.
+        i = switch(fn,
+                   Filter = ,
+                   Find = ,
+                   Map =,
+                   Negate =,
+                   Position =,
+                   Reduce =,
+                   do.call = 2,
+                   sapply =,
+                   lapply =,
+                   vapply = ,
+                   eapply = ,
+                   rapply = 3,               
+                   by = ,
+                   tapply =,
+                   outer = ,
+                   apply = ,
+                   aggregate = 4, # but may be a method.
+                   mapply = 2,
+                   sweep = 5,
+                   NA)
+    }
+    
+    if(is.na(i))
+        return(NA)
+
+    as.character(x[[ i ]] )
 }
 
