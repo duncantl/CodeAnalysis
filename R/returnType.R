e5e02416aa5fddd82fe4ffb487817c26d3bb94ae
Duncan Temple Lang <duncan@r-project.org>
2025-03-18 08:05:09 -0700

    getReturnType.brace and assignType functions

--- R/returnType.R
+++ R/returnType.R
@@ -35,27 +35,8 @@ function(x)
     if(isSimpleAssignTo(x))
         return(getReturnType(x[[3]]))
     
-    if(isCallTo(x, "{")) {
-        y = as.list(x)[-1]
-        last = y[[length(y)]]
-        if(!is.na(ty <- getReturnType(last)))
-            return(ty)
-
-        # now need to look at the earlier calls in this {.
-        # Missing context of calls before that. See if we can get those passed
-        # to this during the walkCode()
-
-        if(isSymbol(last)) {
-            # look for assignments and updates within {, i.e. earlier elements
-
-            vals = findAssignsTo(y[ - length(y) ], as.character(last))
-            # this should probably be a for() loop and we get the return type
-            # of the previous one and update it.
-            ty = sapply(vals, getReturnType)
-            browser()
-        }
-        
-    }
+    if(isCallTo(x, "{")) 
+        return(getReturnType.brace(x))        
 
     if(isCallTo(x, "if")) {
         # if call to "if", return the return types from all branches.
@@ -71,4 +52,83 @@ function(x)
 
     
     NA 
+}
+
+getReturnType.brace =
+function(x)
+{
+    y = as.list(x)[-1]
+    last = y[[length(y)]]
+    if(!is.na(ty <- getReturnType(last)))
+        return(ty)
+
+    # now need to look at the earlier calls in this {.
+    # Missing context of calls before that. See if we can get those passed
+    # to this during the walkCode()
+
+    if(isSymbol(last)) {
+        # look for assignments and updates within {, i.e. earlier elements
+
+        last2 = as.character(last)
+        
+        vals = findAssignsTo(y[ - length(y) ], last2)
+        w = sapply(vals, isSimpleAssignTo)
+        if(any(w)) {
+            i = max(which(w))
+            vals = vals[i:length(vals)]
+        }
+        
+        # this should probably be a for() loop and we get the return type
+        # of the previous one and update it.
+        #         ty = sapply(vals, getReturnType)
+        
+        ty = getReturnType(vals[[1]])
+        if(length(vals) > 1) {
+            browser()
+            for(v in vals[-1]) {
+                # Check if changes type.
+                ty = assignType(v, ty, last2)
+                if(is.na(ty))
+                    break
+            }
+
+            return(ty)
+        }
+    }
+}
+
+assignType =
+    #
+    # x currently expected to be a complex assignment to a variable var
+    # type is the current type of var
+    # var is a character giving the name.
+    #
+    #  environment(fn) = value
+    #  foo(x) = value
+    #  names(x) = value
+    #
+    #
+function(x, type, var)
+{
+    rhs = x[[2]]
+    # is this a simple call fun(var, ...)
+    # or a nested sequence of calls h(g(var, ...))
+    simple = is.call(rhs) && isSymbol(rhs[[1]]) && isSymbol(rhs[[2]], var)
+    if(simple) {
+
+        op = as.character(rhs[[1]])
+        ty = switch(op,
+                    names =,
+                    attr=,
+                    length=,
+                    environment = type,
+                    NA
+                    )
+        return(ty)
+    }
+
+    # can chase down complex calls on rhs   XXX
+
+    # for now just say we don't know the resulting type.
+    NA
 }
