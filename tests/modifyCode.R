397aa46efdda77a714feafa36f8317424bf928d3
Duncan Temple Lang <duncan@r-project.org>
2025-03-18 07:52:58 -0700

    add a test for walking all the objects in the tools package with a no-op modify-code walker to see if they round trip to be identical. This  includes lists, environments, literals,  and many closures. Works.

--- tests/modifyCode.R
+++ tests/modifyCode.R
@@ -18,4 +18,64 @@ z3 = walkCode(tmp, w)
 stopifnot(identical(z3, tmp))
 
 
+if(FALSE) {
+    library(codetools)
+    w = mkModifyCodeWalker(function(x, ...) x)
+    ns = getNamespace("tools")
+    tfns = as.list(ns, all.names = TRUE)
+    # Takes 5 seconds for 794 objects
+    ok = sapply(tfns, function(v) 
+        try(identical(v, walkCode(v, w))))
+
+    table(types <- sapply(tfns, typeof))
+    #
+    #  character     closure      double environment     integer        list     logical 
+    #         26         716           2           5          12          32           1 
+
+
+    # disable stop for now.
+    stopifnot(all(ok))
+    # Originally, 770 TRUE, 24 are not.
+
+    table(types[!ok])
+    v = names(tfns)[!ok]
+    # 21 are C_... and are resolved native routine info object.
+    # Other 4 were 
+    # .standardizable_license_specs_db
+    # IANA_HTTP_status_code_db
+    # IANA_URI_scheme_db
+    # Adobe_glyphs
+    # Comparing
+    #  a = tfns$Adobe_glyphs
+    #  b = walkCode(a, w)
+    #  all.equal(a, b)
+    # we see it is the attributes. Hence, we set attributes(ans) = attributes(x) in
+    # the mkModifyCodeWalker() function for the clause that processes the
+    #   expression/list/language object.
+    #
+    # Not doing this for a pairlist. Setting attributes doesn't "stick" for formals().
+    # Where else do we see pairlist in the wild?
+    #
+    # attr(formals(f), "xyz") = 10
+    # is.null(attr(formals(f), "xyz"))
+    #
+    # But the following yields 10
+    if(FALSE) {
+     p = formals(f)
+     attr(p, "xyz") = 10
+     class(p)
+     attr(p, "xyz") == 10
+    }
+    # 
+    # But the
+    if(FALSE) {
+        formals(f) = p
+        attributes(formals(f))
+        attr(formals(f), "xyz")
+    }
+    
+    #
+}
+
+
 
