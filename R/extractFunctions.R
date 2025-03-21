3883c219cc1999d63e1a11fd3bdd7ef8f34b37de
Duncan Temple Lang <duncan@r-project.org>
2025-03-21 11:02:58 -0700

    added isChainedAssign and rhs/last.   Added skipChained = TRUE/FALSE parameter to findNamedFunctions.  names parameter to isNamedFunctionAssign and removeNamedFunctionDefs.   And extractFunctions checks if nested is empty.

--- R/extractFunctions.R
+++ R/extractFunctions.R
@@ -9,12 +9,17 @@ extractFunctions =
 function(fun)
 {
     nested = findNamedFunctions(fun)
-    fun2 = removeNamedFunctionDefs(fun)
-    list(newFun = fun2, nested = nested)
+    if(length(nested)) {
+        fun2 = removeNamedFunctionDefs(fun, names(nested))
+        list(newFun = fun2, nested = nested)
+    } else
+        list(newFun = fun, nested = list())
+    
 }
 
+
 removeNamedFunctionDefs =
-function(fun)    
+function(fun, names = character())    
 {
     # XXX  this doesn't handle a <- b <- function()...
     # Ends up with a <-  and then NULL and the assignment language object has length 2 but displays as a <- NULL.
@@ -23,8 +28,10 @@ function(fun)
     # XXX  var = if(...)  function() ... else function( ) ... 
     #  also tools::check_doi_db 
     #
-    rw = genRemoveCode(isNamedFunctionAssign)
-    w = mkModifyCodeWalker(rw, FALSE)
+    p = isNamedFunctionAssign
+    formals(p)$names = names
+    rw = genRemoveCode(p)
+    w = mkModifyCodeWalker(rw) # , FALSE)
     walkCode(fun, w)        
 }
 
@@ -50,30 +57,39 @@ isNamedFunctionAssign =
     # CodeAnalysis:::isNamedFunctionAssign(quote( x <- function(x) x+1))
     # CodeAnalysis:::isNamedFunctionAssign(quote( x <- if(a) function(x) x+1 else function(y) y/2))    
     #
-function(x, ...)
+function(x, names = character(), ...)
 {
     w0 = isSimpleAssignTo(x)
     if(!w0)
         return(FALSE)
     
     w =  w0 && isCallTo(x[[3]], "function")
-    if(w)  
+    if(w  && (length(names) == 0 || as.character(x[[2]]) %in% names))
         return(TRUE)
 
     rhs = x[[3]]
     w = isCallTo(rhs, "if") && evalsToFunction(rhs) 
-    if(w)
+    if(w && (length(names) == 0 || as.character(x[[2]]) %in% names))
         return(TRUE)
 
     FALSE
 }
 
-findAssignedFunctions = findNamedFunctions =
+findNamedFunctions =
     # This version finds only function() ...  which are assigned to a simple variable.
-    # So it ignores, e.g., fns$x = function()...aa
-function(fun)
+    # So it ignores, e.g., fns$x = function()...
+    # Could make complex = TRUE/FALSE a parameter
+    #
+    #
+    # h = function() { FOO1 <- FOO2 <- function() 1}
+    # f = function() { FOO1 <- FOO2 <- function() 1; g = function(x) x + 1; g(2)}
+    # findNamedFunctions(h)
+    # findNamedFunctions(g)    
+    #
+    #
+function(fun, skipChained = FALSE)
 {
-    asg = findAssignsTo(fun)
+    asg = findAssignsTo(fun, complex = FALSE)
     if(length(asg) == 0)
         return(list())
     
@@ -84,12 +100,47 @@ function(fun)
     # check if RHS is also a simple assign of a function
     # and the hopefully it is the next one in the list.  Not definitively a match
     # as could  have a <- b <- function () .... and then another b <- function() ...
-    # But really!  Check same function
+    # But really!
+    # Check if either assignment (and there could be more than 2)
 
+
+    if(!skipChained) {
+        w = sapply(asg, isChainedAssign)
+        isFun = isFun & !w
+    }
+        
     funs = lapply(asg[isFun], function(x) x[[3]])
     names(funs) = sapply(asg[isFun], function(x) as.character(x[[2]]))
    
     funs
+}
+
+AssignmentOps = c("<-", "=", "<<-")
+
+isChainedAssign =
+    #
+    # can add parameter for simple/complex assignment.
+    #
+function(x, assignmentOps = AssignmentOps)    
+{
+    !is.null(attr(x, "chainedAssignmentTo")) ||
+      ( isCallTo(x, assignmentOps) && length(x) > 2 && isCallTo(x[[3]], assignmentOps))
+}
+        
+rhs = last =
+    #
+    # to get the RHS of a sequence of a = b = c = d = ..... = value
+    #
+    # rhs(quote(a <- b <- c <- d <- rep(NA, length(y))))
+    # returns rep(NA, length(y))
+    #
+function(x, assignmentOps = c("<-", "=", "<<-"))
+{
+    o = x
+    while(isCallTo(x, assignmentOps) && isSymbol(x[[2]]) && length(x) > 2)
+        x = x[[3]]
+
+    x
 }
 
 
